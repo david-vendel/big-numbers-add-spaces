6e45ed7dfa65c5420a640320a34a8fd9e3f8fe7c
David Vendel <33971649+justdvl@users.noreply.github.com>
2024-07-25 22:31:28 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+This simple extension adds space to every large number, per each 3 digits, for easier readability.
+Changes 12345 -> 12 345.
---
+++ content.js
@@ -0,0 +1,23 @@
+function formatNumberWithSpaces(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+}
+
+function processTextNodes(node) {
+  if (node.nodeType === Node.TEXT_NODE) {
+    const newText = node.nodeValue.replace(/\d+/g, (match) => formatNumberWithSpaces(match));
+    if (newText !== node.nodeValue) {
+      console.log("node.nodeValue",node.nodeValue)
+      node.nodeValue = newText;
+    }
+  } else if (node.nodeType === Node.ELEMENT_NODE && node.childNodes) {
+    node.childNodes.forEach(processTextNodes);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  processTextNodes(document.body);
+});
+
+setTimeout(() => {
+  processTextNodes(document.body);
+}, 1)
